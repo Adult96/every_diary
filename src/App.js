fa9661c6e2a0f15e3e5db9edeca14a9d007f8ddc
Adult96 <djxor007@gmail.com>
2023-02-18 16:32:39 +0900

    faet: 다이어리 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
 function App() {
-  return;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -2,11 +2,10 @@ import axios from 'axios';
 import moment from 'moment';
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
-import './App.css';
-import 'react-calendar/dist/Calendar.css'; // css import
+import 'react-calendar/dist/Calendar.css';
 import styled from 'styled-components';
 
-function App() {
+function CalendarPage() {
   const [value, onChange] = useState(new Date());
 
   const [mark, setMark] = useState(['2023-02-16', '2023-02-17']);
@@ -25,14 +24,107 @@ function App() {
   };
 
   return (
-    <Calendar
-      onChange={onChange}
-      formatDay={(locale, date) => moment(date).format('DD')}
-      value={value}
-      tileContent={show}
-    />
+    <CalendarContainer>
+      <Calendar
+        onChange={onChange}
+        formatDay={(locale, date) => moment(date).format('DD')}
+        value={value}
+        tileContent={show}
+      />
+    </CalendarContainer>
   );
 }
+
+const CalendarContainer = styled.div`
+  .react-calendar {
+    width: 400px;
+    max-width: 100%;
+    background-color: #fff;
+    color: #222;
+    border-radius: 8px;
+    box-shadow: 0 12px 24px rgba(0, 0, 0, 0.2);
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.125em;
+  }
+  .react-calendar__navigation button {
+    color: #6f48eb;
+    min-width: 44px;
+    background: none;
+    font-size: 16px;
+    margin-top: 8px;
+  }
+  .react-calendar__navigation button:enabled:hover,
+  .react-calendar__navigation button:enabled:focus {
+    background-color: #f8f8fa;
+  }
+  .react-calendar__navigation button[disabled] {
+    background-color: #f0f0f0;
+  }
+  abbr[title] {
+    text-decoration: none;
+  }
+  /* .react-calendar__month-view__days__day--weekend {
+  color: #d10000;
+ } */
+  .react-calendar__tile:enabled:hover,
+  .react-calendar__tile:enabled:focus {
+    background: #f8f8fa;
+    color: #6f48eb;
+    border-radius: 6px;
+  }
+  .react-calendar__tile--now {
+    background: #6f48eb33;
+    border-radius: 6px;
+    font-weight: bold;
+    color: #6f48eb;
+  }
+  .react-calendar__tile--now:enabled:hover,
+  .react-calendar__tile--now:enabled:focus {
+    background: #6f48eb33;
+    border-radius: 6px;
+    font-weight: bold;
+    color: #6f48eb;
+  }
+  .react-calendar__tile--hasActive:enabled:hover,
+  .react-calendar__tile--hasActive:enabled:focus {
+    background: #f8f8fa;
+  }
+  .react-calendar__tile--active {
+    background: #6f48eb;
+    border-radius: 6px;
+    font-weight: bold;
+    color: white;
+  }
+  .react-calendar__tile--active:enabled:hover,
+  .react-calendar__tile--active:enabled:focus {
+    background: #6f48eb;
+    color: white;
+  }
+  .react-calendar--selectRange .react-calendar__tile--hover {
+    background-color: #f8f8fa;
+  }
+  .react-calendar__tile--range {
+    background: #f8f8fa;
+    color: #6f48eb;
+    border-radius: 0;
+  }
+  .react-calendar__tile--rangeStart {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+    border-top-left-radius: 6px;
+    border-bottom-left-radius: 6px;
+    background: #6f48eb;
+    color: white;
+  }
+  .react-calendar__tile--rangeEnd {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    border-top-right-radius: 6px;
+    border-bottom-right-radius: 6px;
+    background: #6f48eb;
+    color: white;
+  }
+`;
 
 const DotBox = styled.div`
   position: relative;
@@ -52,4 +144,4 @@ const Dot = styled.div`
   margin-left: 1px;
 `;
 
-export default App;
+export default CalendarPage;
