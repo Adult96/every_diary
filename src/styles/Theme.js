f191ea4107c4d0f18922cb28487a034feeadb4b1
Adult96 <djxor007@gmail.com>
2023-02-20 18:17:16 +0900

    style: theme 파일에서 현재 사용하지 않는 코드 주석처리

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -22,27 +22,27 @@ const color = {
   trans: 'transparent',
 };
 
-const fontSize = {
-  large: '3rem',
-  medium: '1.75rem',
-  regular: '1.125rem',
-  small: '1rem',
-  micro: '0.875rem',
-};
+// const fontSize = {
+//   large: '3rem',
+//   medium: '1.75rem',
+//   regular: '1.125rem',
+//   small: '1rem',
+//   micro: '0.875rem',
+// };
 
-const fontWeight = {
-  bold: 700,
-  semi_bold: 600,
-  regular: 400,
-};
+// const fontWeight = {
+//   bold: 700,
+//   semi_bold: 600,
+//   regular: 400,
+// };
 
-const borderRadius = {
-  round: '100%',
-  half_round: '50%',
-  large: '3rem',
-  medium: '1.5rem',
-  small: '0.5rem',
-};
+// const borderRadius = {
+//   round: '100%',
+//   half_round: '50%',
+//   large: '3rem',
+//   medium: '1.5rem',
+//   small: '0.5rem',
+// };
 
 const light = {
   bg: color.white,
