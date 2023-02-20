76b7737783349411ad37f432da1656799877178f
Adult96 <djxor007@gmail.com>
2023-02-21 05:27:18 +0900

    배포 업데이트

--- server.js
+++ server.js
@@ -1,47 +1,27 @@
 const jsonServer = require('json-server');
-const path = require('path');
+const clone = require('clone');
+const data = require('./db.json');
 
+const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create();
-const router = jsonServer.router(path.resolve(__dirname + '/db.json'));
-const middlewares = jsonServer.defaults({
-  static: path.resolve(__dirname + '/../build/'),
-});
 
-const port = process.env.PORT || 3001;
+// For mocking the POST request, POST request won't make any changes to the DB in production environment
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
+  _isFake: isProductionEnv,
+});
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
-server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.path !== '/') router.db.setState(clone(data));
+  next();
+});
 
 server.use(router);
-server.listen(port, () => {
+server.listen(process.env.PORT || 8000, () => {
   console.log('JSON Server is running');
 });
 
-// const jsonServer = require('json-server');
-// const clone = require('clone');
-// const data = require('./db.json');
-
-// const isProductionEnv = process.env.NODE_ENV === 'production';
-// const server = jsonServer.create();
-
-// // For mocking the POST request, POST request won't make any changes to the DB in production environment
-// const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
-//   _isFake: isProductionEnv,
-// });
-// const middlewares = jsonServer.defaults();
-
-// server.use(middlewares);
-
-// server.use((req, res, next) => {
-//   if (req.path !== '/') router.db.setState(clone(data));
-//   next();
-// });
-
-// server.use(router);
-// server.listen(process.env.PORT || 8000, () => {
-//   console.log('JSON Server is running');
-// });
-
-// // Export the Server API
-// module.exports = server;
+// Export the Server API
+module.exports = server;
