2ca55e5d509a4a2f7654b3823346315625ca0f1b
Adult96 <djxor007@gmail.com>
2023-02-21 05:47:31 +0900

    배포 설정

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const jsonServer = require('json-server');
-const clone = require('clone');
-const data = require('./db.json');
+const path = require('path');
 
-const isProductionEnv = process.env.NODE_ENV === 'production';
-const server = jsonServer.create();
+const fs = require('fs');
+const os = require('os');
 
-// For mocking the POST request, POST request won't make any changes to the DB in production environment
-const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
-  _isFake: isProductionEnv,
+fs.copyFile('db.json', os.tmpdir() + '/db.json', function (err) {
+  if (err) console.log(err);
+  else console.log('copy file succeed to' + os.tmpdir());
 });
-const middlewares = jsonServer.defaults();
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.resolve(os.tmpdir() + '/db.json'));
+const middlewares = jsonServer.defaults({
+  static: path.resolve(__dirname + '/../build/'),
+});
+
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 
-server.use((req, res, next) => {
-  if (req.path !== '/') router.db.setState(clone(data));
-  next();
-});
+server.use(jsonServer.bodyParser);
 
 server.use(router);
-server.listen(process.env.PORT || 8000, () => {
+server.listen(port, () => {
   console.log('JSON Server is running');
 });
-
-// Export the Server API
-module.exports = server;
