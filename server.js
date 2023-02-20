693a0b799494d97579a5ed97196babccfa14345b
Adult96 <djxor007@gmail.com>
2023-02-21 05:41:03 +0900

    배포 수정

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const jsonServer = require('json-server');
 const path = require('path');
 
+const fs = require('fs');
+const os = require('os');
+
+fs.copyFile('db.json', os.tmpdir() + '/db.json', function (err) {
+  if (err) console.log(err);
+  else console.log('copy file succeed to' + os.tmpdir());
+});
+
 const server = jsonServer.create();
-const router = jsonServer.router(path.resolve(__dirname + '/db.json'));
+const router = jsonServer.router(path.resolve(os.tmpdir() + '/db.json'));
 const middlewares = jsonServer.defaults({
   static: path.resolve(__dirname + '/../build/'),
 });
