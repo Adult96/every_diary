da84fa07b339b8f83fc72b3f75a4cb8d73815a4b
Adult96 <djxor007@gmail.com>
2023-02-20 14:12:38 +0900

    style: item text style set

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -5,8 +5,8 @@ export default function DiaryItem({ diary }) {
   return (
     <Item>
       <TextBox>
-        <p>{diary.title}</p>
-        <p>{diary.content}</p>
+        <Title>{diary.title}</Title>
+        <Content>{diary.content}</Content>
       </TextBox>
     </Item>
   );
@@ -23,4 +23,28 @@ const Item = styled.li`
 
 const TextBox = styled.div`
   height: 90%;
+  /* gird박스의 width가 동적이기 때문에 
+    word-wrap과 word-break로 박스 밖을 넘어가지 않게 설정
+  */
+  word-wrap: break-word;
+  word-break: break-all;
+`;
+
+const Title = styled.div`
+  display: -webkit-box;
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  white-space: normal;
+  -webkit-line-clamp: 1; /* 텍스트를 자를 때 원하는 단위 ex) 3줄 */
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+`;
+
+const Content = styled.div`
+  display: -webkit-box;
+  white-space: normal;
+  -webkit-line-clamp: 9; /* 텍스트를 자를 때 원하는 단위 ex) 3줄 */
+  -webkit-box-orient: vertical;
+  overflow: hidden;
 `;
